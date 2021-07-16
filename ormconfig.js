9dcf41534de98096af5413365e368b87a9f7a959
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-07-16 10:11:35 -0300

    test

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    "entities": ["src/entities/*.ts"],
+    "entities": ["dist/entities/*.js"],
     "cli": {
         "migrationsDir": "src/database/migrations",
         "entitiesDir": "src/entities"
